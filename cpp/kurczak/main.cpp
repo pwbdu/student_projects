0588d234e76a7a94a519c07457fc21b9f3025ce1
pwbdu <student@student.machine>
2024-10-24 14:01:58 +0000

    studentmachine send

---
+++ cpp/kurczak/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+float eee;
+cout << "podaj liczbe kór"<<endl;
+cin >> eee;
+float fff;
+cout<<"podaj liczbe kogutuw";
+cin >> fff;
+float rrr=eee*fff*'pi';
+cout << "liczba kórczakuw ="<< rrr;
+}
+return 0;
